cbe32a1dc07e1e8d31751760c7a29781402b9c0e
deram <deram@iki.fi>
2013-02-09 03:09:21 +0200

    cleanup anr refactoring the clock

--- graphics.py
+++ graphics.py
@@ -1,3 +1,4 @@
+import cocos
 from cocos.director import director
 from cocos.layer import Layer, ColorLayer
 from cocos.scene import Scene
@@ -14,52 +15,35 @@ from pyglet.window import key
 
 from isk_presenter import CurrentPresenter
 
-class OutlineLabel(pyglet.text.Label):
+class OutlineLabel(cocos.text.Label):
     def __init__( self, *args, **kwargs):
         outline_color = kwargs.pop('outline_color')
         super(OutlineLabel, self).__init__(*args, **kwargs)
         x = kwargs.pop('x')
         y = kwargs.pop('y')
         kwargs.pop('color')
-        self.outline=[]
         for dx in (-2,2):
             for dy in (-2,2):
-                self.outline.append(pyglet.text.Label(*args, x=x+dx, y=y+dy, 
-                                                      color=outline_color, 
-                                                      **kwargs))
-    def draw(self):
-        for item in self.outline:
-            item.draw()
-        super(OutlineLabel, self).draw()
+                self.add(cocos.text.Label(*args, x=x+dx, y=y+dy, color=outline_color, **kwargs), z=-1)
 
     def set_style(self, *args, **kwargs):
-        super(OutlineLabel, self).set_style(*args, **kwargs)
-        for item in self.outline:
-            item.set_style(*args, **kwargs)
+        self.walk(lambda item: item.element.set_style(*args, **kwargs))
 
-    def begin_update(self):
-        super(OutlineLabel, self).begin_update()
-        for item in self.outline:
-            item.begin_update()
+    def set_text(self, text):
+        def set_element_text(item):
+            item.element.begin_update()
+            item.element.text=text
+            item.element.end_update()
 
-    def end_update(self):
-        super(OutlineLabel, self).end_update()
-        for item in self.outline:
-            item.text=self.text
-            item.end_update()
+        self.walk(set_element_text)
+        
 
-
-class ControlLayer(Layer):
-
-    is_event_handler = True     #: enable pyglet's events
-
-    def __init__( self, clock=True ):
-        super(ControlLayer, self).__init__()
-        self.scheduled_event=False
-        self.push_all_handlers()
-        self.clock=clock
+class ClockLayer(Layer):
+    def __init__(self):
+        super(ClockLayer, self).__init__()
         now=datetime.now()
-        self.text_title = OutlineLabel(text=now.strftime("%H:%M:%S") ,
+        self.add(OutlineLabel(
+            text=now.strftime("%H:%M:%S") ,
             color=(255,255,255,255),
             font_size=40,
             font_name="Franklin Gothic Heavy",
@@ -68,31 +52,27 @@ class ControlLayer(Layer):
             y=130,
             anchor_x=font.Text.RIGHT,
             anchor_y=font.Text.TOP,
-            outline_color=(0,50,0,255) )
-        self.text_title.set_style('kerning', 2)
+            outline_color=(0,0,0,255) ), name='label')
+        self.get('label').set_style('kerning', 2)
+        
+        self.schedule(self.change_time)
 
-    def draw( self ):
-        if (self.clock):
-            self.text_title.draw()
+    def change_time(self, dt=0):
+        now=datetime.now()
+        self.get('label').set_text(now.strftime("%H:%M:%S"))
+
+class ControlLayer(Layer):
+
+    is_event_handler = True     #: enable pyglet's events
+
+    def __init__( self):
+        super(ControlLayer, self).__init__()
+        self.push_all_handlers()
 
     def on_key_press( self, k , m ):
         if k == key.ENTER:
             self.do(CallFunc(self.parent.change_slide))
             return True
-
-    def on_enter(self):
-	if (not self.scheduled_event):
-            self.scheduled_event = True
-            if (self.clock):
-                self.schedule(self.change_time)
-        return super(ControlLayer, self).on_enter()
-
-    def change_time(self, dt=0):
-        now=datetime.now()
-        self.text_title.begin_update()
-        self.text_title.text=now.strftime("%H:%M:%S")
-        self.text_title.end_update()
-
 
 
 class SlideLayer(Layer):
@@ -123,8 +103,7 @@ class VideoLayer (SlideLayer):
 
     def handle_on_eos(self):
         if ( not self.eos_handled ):
-            transition=FadeBLTransition #FadeTransition
-            director.replace(transition(SlideScene(), 1.25))
+            self.do(CallFunc(self.parent.change_slide))
         self.eos_handled=True
         return True
 
@@ -155,6 +134,7 @@ class SlideScene(Scene):
             self.filename=filename
             self.duration=100
             self.clock=True
+            self.type="video"
             
         self.add(ColorLayer(255,255,255,255), z=-10)
 
@@ -163,13 +143,13 @@ class SlideScene(Scene):
 	else:
             self.add(SlideLayer(self.filename), z=0)
 
-        self.add(ControlLayer(self.clock), z=10)
+        if (self.clock):
+            self.add(ClockLayer(), z=5)
 
-    def on_enter(self):
-	if (not self.scheduled_event and self.duration > 0):
-            self.scheduled_event = True
+        self.add(ControlLayer(), z=10)
+
+	if (self.duration > 0):
             self.schedule_interval(self.change_slide, self.duration)
-        return super(SlideScene, self).on_enter()
 
     def change_slide(self, dt=0):
         transition=FadeBLTransition #FadeTransition
