7eaa87f5cc7827c92f0c9f4b816e69c2952d488a
deram <deram@iki.fi>
2013-02-14 01:38:44 +0200

    changed default config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ window=dict(width=1280, height=720, resizable=True)
 scale_down=False
 fullscreen=True
 
+default_duration=5
 
 displayid=1
 display_name="Sali-av-isk"
@@ -9,9 +10,3 @@ cache_path="cache"
 server="http://85.188.56.129/"
 user="isk"
 passwd="Kissa"
-
-#import resource
-#resource.setrlimit(resource.RLIMIT_AS, (100 * 1048576L, -1L))
-#resource.setrlimit(resource.RLIMIT_DATA, (100 * 1048576L, -1L))
-#import gc
-#gc.set_debug(gc.DEBUG_UNCOLLECTABLE | gc.DEBUG_INSTANCES | gc.DEBUG_OBJECTS)
