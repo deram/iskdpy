0d59f69ef22286d20fbace039c2e46126b0b0986
deram <deram@iki.fi>
2013-02-18 21:33:17 +0200

    clock configs

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ window=dict(width=1280, height=720, resizable=True)
 scale_down=False
 fullscreen=True
 
+clock=dict(x=900, y=10, format="%a %H:%M:%S", font="Franklin Gothic Heavy")
+
 default_duration=5
 
 displayid=1
-display_name="Sali-av-isk"
+display_name="deram-test"
 cache_path="cache"
-server="http://85.188.56.129/"
+server="http://isk0.asm.fi/"
 user="isk"
 passwd="Kissa"
