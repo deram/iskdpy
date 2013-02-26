0b52b668458baf64e573b4b3dcf69afabd1b820e
deram <deram@iki.fi>
2013-02-26 22:25:39 +0200

    modified to use hardtabs

--- graphics.py
+++ graphics.py
@@ -18,144 +18,144 @@ import config
 import isk_presenter
 
 class OutlineLabel(cocos.text.Label):
-    def __init__( self, *args, **kwargs):
-        self.text=None
-        outline_color = kwargs.pop('outline_color')
-        super(OutlineLabel, self).__init__(*args, **kwargs)
-        x = kwargs.pop('x')
-        y = kwargs.pop('y')
-        kwargs.pop('color')
-        for dx in (-2,2):
-            for dy in (-2,2):
-                self.add(cocos.text.Label(*args, x=x+dx, y=y+dy, color=outline_color, **kwargs), z=-1)
+	def __init__( self, *args, **kwargs):
+		self.text=None
+		outline_color = kwargs.pop('outline_color')
+		super(OutlineLabel, self).__init__(*args, **kwargs)
+		x = kwargs.pop('x')
+		y = kwargs.pop('y')
+		kwargs.pop('color')
+		for dx in (-2,2):
+			for dy in (-2,2):
+				self.add(cocos.text.Label(*args, x=x+dx, y=y+dy, color=outline_color, **kwargs), z=-1)
 
-    def set_style(self, *args, **kwargs):
-        self.walk(lambda item: item.element.set_style(*args, **kwargs))
+	def set_style(self, *args, **kwargs):
+		self.walk(lambda item: item.element.set_style(*args, **kwargs))
 
-    def set_text(self, text):
-        def set_element_text(item):
-            item.element.begin_update()
-            item.element.text=text
-            item.element.end_update()
+	def set_text(self, text):
+		def set_element_text(item):
+			item.element.begin_update()
+			item.element.text=text
+			item.element.end_update()
 
-        if (self.text != text):
-            self.walk(set_element_text)
-            self.text=text
+		if (self.text != text):
+			self.walk(set_element_text)
+			self.text=text
 
 class ClockLayer(Layer):
-    def __init__(self):
-        super(ClockLayer, self).__init__()
-        self.time=datetime.now()
-        self.clock_format=config.clock['format']
+	def __init__(self):
+		super(ClockLayer, self).__init__()
+		self.time=datetime.now()
+		self.clock_format=config.clock['format']
 
-        self.add(OutlineLabel(
-            text=self.time.strftime(self.clock_format) ,
-            color=(255,255,255,255),
-            font_size=40,
-            font_name=config.clock['font'],
-            bold=True,
-            x=config.clock['x'],
-            y=config.clock['y'],
-            anchor_x=font.Text.LEFT,
-            anchor_y=font.Text.BOTTOM,
-            outline_color=(0,0,0,255) ), name='label')
+		self.add(OutlineLabel(
+			text=self.time.strftime(self.clock_format) ,
+			color=(255,255,255,255),
+			font_size=40,
+			font_name=config.clock['font'],
+			bold=True,
+			x=config.clock['x'],
+			y=config.clock['y'],
+			anchor_x=font.Text.LEFT,
+			anchor_y=font.Text.BOTTOM,
+			outline_color=(0,0,0,255) ), name='label')
 
-        self.get('label').set_style('kerning', 2)
-        
-        self.schedule_interval(self.change_time, 0.2)
+		self.get('label').set_style('kerning', 2)
+		
+		self.schedule_interval(self.change_time, 0.2)
 
-    def change_time(self, dt=0):
-        self.time=datetime.now()
-        self.get('label').set_text(self.time.strftime(self.clock_format))
+	def change_time(self, dt=0):
+		self.time=datetime.now()
+		self.get('label').set_text(self.time.strftime(self.clock_format))
 
 class ControlLayer(Layer):
 
-    is_event_handler = True     #: enable pyglet's events
+	is_event_handler = True	 #: enable pyglet's events
 
-    def __init__( self):
-        super(ControlLayer, self).__init__()
-        self.push_all_handlers()
+	def __init__( self):
+		super(ControlLayer, self).__init__()
+		self.push_all_handlers()
 
-    def on_key_press( self, k , m ):
-        if k == key.ENTER:
-            self.do(CallFunc(self.parent.change_slide))
-            return True
+	def on_key_press( self, k , m ):
+		if k == key.ENTER:
+			self.do(CallFunc(self.parent.change_slide))
+			return True
 
 
 class SlideLayer(Layer):
-    def __init__( self, file):
-        super( SlideLayer, self ).__init__()
-        try:
-            g = Sprite( file, anchor=(0,0) )
-        except pyglet.image.codecs.ImageDecodeException:
-            self.invalid = True
-        else:
-            self.add( g )
+	def __init__( self, file):
+		super( SlideLayer, self ).__init__()
+		try:
+			g = Sprite( file, anchor=(0,0) )
+		except pyglet.image.codecs.ImageDecodeException:
+			self.invalid = True
+		else:
+			self.add( g )
 
 class VideoLayer (SlideLayer):
-    def __init__(self, video_name):
-        super(SlideLayer, self).__init__()
+	def __init__(self, video_name):
+		super(SlideLayer, self).__init__()
 
-        source = pyglet.media.load(video_name)
-        format = source.video_format
-        if not format:
-            print 'No video track in this source.'
-            return
+		source = pyglet.media.load(video_name)
+		format = source.video_format
+		if not format:
+			print 'No video track in this source.'
+			return
 
-        self.media_player = pyglet.media.Player()
-        self.media_player.queue(source)
-        self.media_player.eos_action=self.media_player.EOS_PAUSE
-        self.media_player.set_handler('on_eos', self.handle_on_eos)
-        self.eos_handled=False
+		self.media_player = pyglet.media.Player()
+		self.media_player.queue(source)
+		self.media_player.eos_action=self.media_player.EOS_PAUSE
+		self.media_player.set_handler('on_eos', self.handle_on_eos)
+		self.eos_handled=False
 
-    def handle_on_eos(self):
-        if ( not self.eos_handled ):
-            self.do(CallFunc(self.parent.change_slide))
-        self.eos_handled=True
-        return True
+	def handle_on_eos(self):
+		if ( not self.eos_handled ):
+			self.do(CallFunc(self.parent.change_slide))
+		self.eos_handled=True
+		return True
 
-    def on_enter(self):
-        self.media_player.play()
-        return super(VideoLayer, self).on_enter()
+	def on_enter(self):
+		self.media_player.play()
+		return super(VideoLayer, self).on_enter()
 
-    def on_exit(self):
-        self.media_player.pause()
-        return super(VideoLayer, self).on_exit()
+	def on_exit(self):
+		self.media_player.pause()
+		return super(VideoLayer, self).on_exit()
 
-    def draw(self):
-        self.media_player.get_texture().blit(0, 0)
+	def draw(self):
+		self.media_player.get_texture().blit(0, 0)
 
 
 
 class SlideScene(Scene):
-    def __init__(self, slide=None):
-        super(SlideScene, self).__init__()
-        self.scheduled_event=False
-        if (not slide):
-       	    slide=isk_presenter.Presenter.current().get_next()
+	def __init__(self, slide=None):
+		super(SlideScene, self).__init__()
+		self.scheduled_event=False
+		if (not slide):
+	   		slide=isk_presenter.Presenter.current().get_next()
 
-        self.filename=slide.get_filename()
-        self.duration=slide.get_duration()
-        self.clock=slide.get_clock()
-        self.type=slide.get_type()
-            
-        self.add(ColorLayer(0,0,0,255), z=-10)
+		self.filename=slide.get_filename()
+		self.duration=slide.get_duration()
+		self.clock=slide.get_clock()
+		self.type=slide.get_type()
+			
+		self.add(ColorLayer(0,0,0,255), z=-10)
 
-	if (self.type=="video"):
-            self.add(VideoLayer(self.filename), z=0)
-	else:
-            self.add(SlideLayer(self.filename), z=0)
+		if (self.type=="video"):
+			self.add(VideoLayer(self.filename), z=0)
+		else:
+			self.add(SlideLayer(self.filename), z=0)
 
-        if (self.clock):
-            self.add(ClockLayer(), z=5)
+		if (self.clock):
+			self.add(ClockLayer(), z=5)
 
-        self.add(ControlLayer(), z=10)
+		self.add(ControlLayer(), z=10)
 
-	if (self.duration > 0):
-            self.schedule_interval(self.change_slide, self.duration)
+		if (self.duration > 0):
+			self.schedule_interval(self.change_slide, self.duration)
 
-    def change_slide(self, dt=0):
-        transition=FadeBLTransition #FadeTransition
-        director.replace(transition(SlideScene(), 1.25))
+	def change_slide(self, dt=0):
+		transition=FadeBLTransition #FadeTransition
+		director.replace(transition(SlideScene(), 1.25))
 
 
--- isk_transitions.py
+++ isk_transitions.py
@@ -8,33 +8,33 @@ from cocos.sprite import Sprite
 from cocos.scenes.transitions import TransitionScene
 
 class FadeTRTransition(TransitionScene):
-    '''Fade the tiles of the outgoing scene from the left-bottom corner the to top-right corner.
-    '''
-    def __init__( self, *args, **kwargs ):
-        super(FadeTRTransition, self ).__init__( *args, **kwargs)
+	'''Fade the tiles of the outgoing scene from the left-bottom corner the to top-right corner.
+	'''
+	def __init__( self, *args, **kwargs ):
+		super(FadeTRTransition, self ).__init__( *args, **kwargs)
 
-        width, height = director.get_window_size()
-        aspect = width / float(height)
-        x,y = int(9*aspect), 9
+		width, height = director.get_window_size()
+		aspect = width / float(height)
+		x,y = int(9*aspect), 9
 
-        a = self.get_action(x,y)
+		a = self.get_action(x,y)
 
-#        a = Accelerate( a)
-        self.out_scene.do( a + \
-                            CallFunc(self.finish) + \
-                            StopGrid() )
+#		a = Accelerate( a)
+		self.out_scene.do( a + \
+							CallFunc(self.finish) + \
+							StopGrid() )
 
-    def start(self):
-        # don't call super. overriding order
-        self.add( self.in_scene, z=0 )
-        self.add( self.out_scene, z=1 )
+	def start(self):
+		# don't call super. overriding order
+		self.add( self.in_scene, z=0 )
+		self.add( self.out_scene, z=1 )
 
-    def get_action(self,x,y):
-        return FadeOutTRTiles( grid=(x,y), duration=self.duration )
+	def get_action(self,x,y):
+		return FadeOutTRTiles( grid=(x,y), duration=self.duration )
 
 class FadeBLTransition(FadeTRTransition):
-    '''Fade the tiles of the outgoing scene from the top-right corner to the bottom-left corner.
-    '''
-    def get_action(self,x,y):
-        return FadeOutBLTiles( grid=(x,y), duration=self.duration )
+	'''Fade the tiles of the outgoing scene from the top-right corner to the bottom-left corner.
+	'''
+	def get_action(self,x,y):
+		return FadeOutBLTiles( grid=(x,y), duration=self.duration )
 
