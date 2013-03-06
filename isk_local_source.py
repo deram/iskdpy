106e750ed71ab05279d871a44caeec5f9931b7db
deram <deram@iki.fi>
2013-03-06 23:56:41 +0200

    modified to fit new config structures. fixed bug: always updating

--- isk_local_source.py
+++ isk_local_source.py
@@ -1,4 +1,3 @@
-import config
 import file
 import glob
 
@@ -9,10 +8,10 @@ register=Source.register
 
 @register('LocalSource')
 class LocalSource(Source):
-	def __init__(self, local_dir="local", display_name=config.display_name):
+	def __init__(self, config):
 		super(LocalSource, self).__init__()
-		self.local_dir=local_dir
-		self.display_name=display_name
+		self.local_dir=config['local_dir']
+		self.display_name=config['display_name']
 		self.files=""
 
 	#def get_display():
@@ -20,14 +19,17 @@ class LocalSource(Source):
 	def update_display(self):
 		files=glob.glob('%s/*.*' % self.local_dir)
 		if ( self.files != files ):
+			print "Contents of '%s' changed, rebuilding display..." % (self.local_dir)
+			self.files=files
 			slides=[]
 			groups=[]
 			for item in files:
-				slides.append(Slide({'filename':item, 'position':len(slides)+1}))
+				slides.append(Slide({'filename':item, 'position':len(slides)+1, 'updated_at':1}))
 			groups.append(Group(slides=slides))
 			presentation=Presentation(groups=groups, attribs={'total_slides':len(slides)})
 			self.display=Display(presentation=presentation)
-		return True
+			return True
+		return False
 
 	def update_slide(self, slide):
 		return slide
