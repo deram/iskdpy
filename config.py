282df1a07953178987ea138323f8722e9eff199c
deram <deram@iki.fi>
2013-03-07 01:00:50 +0200

    default to fullscreen mode

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Default configs override with local_config.py
 window=dict(width=1280, height=720, resizable=True)
-scale_down=True
-fullscreen=False
+scale_down=False
+fullscreen=True
 
 clock=dict(x=900, y=10, format="%a %H:%M:%S", font="Franklin Gothic Heavy")
 empty_slide=dict(filename= 'base.png', duration=10, type= 'image', clock=False)
