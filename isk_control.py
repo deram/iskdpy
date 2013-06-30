3db01fa75d83d37339f9d03435e5412326ee3491
deram <deram@iki.fi>
2013-06-30 05:13:17 +0300

    singletonized control classes

--- isk_control.py
+++ isk_control.py
@@ -4,12 +4,12 @@ from pyglet.window import key
 
 from isk_presenter import Presenter
 
-class KeyboardControlLayer(Layer):
+class _KeyboardControlLayer(Layer):
 
 	is_event_handler = True  #: enable pyglet's events
 
 	def __init__( self):
-		super(KeyboardControlLayer, self).__init__()
+		super(_KeyboardControlLayer, self).__init__()
 		self.push_all_handlers()
 
 	def on_key_press( self, k , m ):
@@ -18,16 +18,26 @@ class KeyboardControlLayer(Layer):
 			return True
 
 
-class RemoteControlLayer(Layer):
+class _RemoteControlLayer(Layer):
 	def __init__(self, *args, **kwargs):
+		super(_RemoteControlLayer, self).__init__(*args, **kwargs)
 		self.source=Presenter.current().get_source()
-		super(RemoteControlLayer, self).__init__(*args, **kwargs)
-
-		self.schedule(self.run)
+		self.source.register_control(self)
+		self.schedule_interval(self.run, 0.1)
 
 	def run(self, *args):
-		ctrl=self.source.get_control()
-		if ctrl:
-			self.do(CallFunc(self.parent.change_slide))
-			print ctrl
+		self.source.run_control()
 
+__rcl=None
+def RemoteControlLayer():
+	global __rcl
+	if not __rcl:
+		__rcl=_RemoteControlLayer()
+	return __rcl
+
+__kcl=None
+def KeyboardControlLayer():
+	global __kcl
+	if not __kcl:
+		__kcl=_KeyboardControlLayer()
+	return __kcl
