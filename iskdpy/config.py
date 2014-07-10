70f70745e86c58b5482ed27ee9f0bcb1660b91fa
deram <deram@iki.fi>
2014-07-10 23:53:17 +0300

    default config revisited
    default to 1080p
    Arial clock.
    added future dict for behavior modification alternatives

--- iskdpy/config.py
+++ iskdpy/config.py
@@ -1,10 +1,10 @@
 # Default configs override with local_config.py
-window=dict(width=1280, height=720, resizable=True)
+window=dict(width=1920, height=1080, resizable=True)
 scale_down=False
 fullscreen=True
 
-clock=dict(x=970, y=12, format="%a %H:%M:%S", size=32, font="Franklin Gothic Heavy")
-empty_slide=dict(filename= 'base.png', duration=10, type= 'image', show_clock=False)
+clock=dict(x=1450, y=12, format="%a %H:%M:%S", size=48, font="Arial")
+empty_slide=dict(filename= 'base.png', duration=10, type= 'image', show_clock=True)
 
 default_duration=5
 default_cache_path='cache'
@@ -18,6 +18,8 @@ logger_levels=dict( iskdpy='INFO' )
 sources=[ dict( source_name='LocalSource', display_name="test_display", local_dir="local") ]
 output="CocosOutput"
 font_files=[]
+
+future={}
 # local configuration overrides defaults
 try:
 	from local_config import *
