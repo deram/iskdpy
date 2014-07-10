d148794dc8768be1b6db945a7808f94919f5b325
deram <deram@iki.fi>
2014-07-11 00:06:00 +0300

    cleanup and refactoring

--- iskdpy/presenter.py
+++ iskdpy/presenter.py
@@ -96,14 +96,13 @@ def run():
 
 	output_ret.get()
 
+def get_empty_slide():
+	return OverrideSlide(**config.empty_slide)
 
 def _connect(conf):
 	source = SourcePlugin.factory(conf.pop('source_name'), conf)
 	if source.connect():
-		with source.get_display() as disp:
-			if disp:
-				_state.display=disp
-				_seek_to_presentation_beginning()
+		pass
 
 def _next_source():
 	if len(config.sources):
@@ -171,53 +170,22 @@ def _set_updated_display(new_display):
 		return True
 	return False
 
-def _seek_to_presentation_beginning():
-	_state.pos = -1
-
-def _get_presentation():
-	return _state.display.presentation
-
-def _get_current_slide():
-	try:
-		return _get_presentation()[_state.pos]
-	except (IndexError, AttributeError, TypeError):
-		logger.warning("Current slide not existing")
-		return Slide()
-
 def _set_current_slide(gid, sid):
-	try:
-		new_pos = _get_presentation().locate_slide(sid, gid)
+	_state.current_slide=Slide(id=sid, group=gid)
+	return _state.current_slide.id==sid and _state.current_slide.group==gid
 
-		if _get_presentation()[new_pos].valid:
-			_state.pos=new_pos
-			return True
-	except (IndexError, AttributeError, TypeError):
-		logger.warning("Slide not in presentationi (%s,%s). Not changing.", gid, sid)
-	return False
-
-def _seek_to_next_valid_slide_in_presentation():
-	n_slides = len(_get_presentation())
-	for _ in xrange(_get_presentation().total_slides):
-		_state.pos += 1
-		if ( _state.pos >= n_slides ):
-			_state.pos = 0
+def _seek_to_valid_slide_in_presentation(dir='next'):
+	delta={'next':1, 'previous':-1}[dir]
+	n_slides = len(_state.presentation)
+	pos=_state.pos
+	for _ in xrange(n_slides):
+		pos = (pos + delta) % n_slides
+		if pos == 0:
 			logger.info("Presentation wrapped")
-		logger.debug("Next: %s", unicode(_get_current_slide()).split('\n', 1)[0])
+		logger.debug("Next: %s", unicode(_state.presentation[pos]))
 
-		if _get_current_slide().valid:
-			return True
-	return False
-
-def _seek_to_previous_valid_slide_in_presentation():
-	n_slides = len(_get_presentation())
-	for _ in xrange(_get_presentation().total_slides):
-		_state.pos -= 1
-		if ( _state.pos < 0 ):
-			_state.pos = n_slides - 1
-			logger.info("Presentation wrapped")
-		logger.debug("Prev: %s", unicode(_get_current_slide()).split('\n', 1)[0])
-
-		if _get_current_slide().valid:
+		if _state.presentation[pos].valid:
+			_state.pos=pos
 			return True
 	return False
 
@@ -247,22 +215,20 @@ def _get_next():
 def _get_previous():
 	return _get_slide('previous')
 
-def _get_slide(slide='next'):
+def _get_slide(dir='next'):
 	if not _state.display:
 		if not _update_display():
 			logger.error("NO DISPLAY FROM SOURCE")
-			return Slide()
+			return get_empty_slide()
 	if ( _is_override() ):
 		ret = _pop_override_slide()
 	else:
 		if (_is_empty_presentation()):
 			logger.warning("EMPTY PRESENTATION")
-			return Slide()
-		if slide=='next':
-			_seek_to_next_valid_slide_in_presentation()
-		elif slide=='previous':
-			_seek_to_previous_valid_slide_in_presentation()
-		ret = _get_current_slide()
+			return get_empty_slide()
+		if dir=='next' or dir=='previous':
+			_seek_to_valid_slide_in_presentation(dir)
+		ret = _state.current_slide
 
 	logger.debug("Next: %s", ret)
 	return ret
@@ -279,6 +245,7 @@ def _schedule_slide_change(duration=1):
 		_state.timer.start()
 
 def _show_slide(slide):
+	logger.debug("_show_slide(%.100s)" % slide)
 	_cancel_slide_change()
 
 	if not slide.ready:
@@ -289,29 +256,35 @@ def _show_slide(slide):
 		if not slide.uptodate:
 			with get_source().update_slide(slide):
 				pass
-		output.set_slide(slide)
-		get_source().slide_done(slide)
+		with output.set_slide(slide):
+			get_source().slide_done(slide)
 
 	duration=slide.duration
 	if duration > 0 and not _is_manual():
 		_schedule_slide_change(duration)
 
-def display_updated():
+def display_updated(display=None):
+	logger.debug("display_updated(%.100s)" % display)
+	if display:
+		return _set_updated_display(display)
 	return _update_display()
 
 def goto_next_slide():
+	logger.debug("goto_next_slide()")
 	slide=_get_next()
 	_show_slide(slide)
 	return True
 
 def goto_previous_slide():
+	logger.debug("goto_previous_slide()")
 	slide=_get_previous()
 	_show_slide(slide)
 	return True
 
 def goto_slide(gid, sid):
+	logger.debug("goto_slide(G_%s, S_%s)" % (gid, sid))
 	if _set_current_slide(gid, sid):
-		slide=_get_current_slide()
+		slide=_state.current_slide
 		_show_slide(slide)
 		return True
 	return False
@@ -322,6 +295,9 @@ def get_source():
 	return SourcePlugin.get_current()
 
 def refresh_slide_cache(slide):
+	logger.debug("refresh_slide_cache(%.100s)" % slide)
 	with OutputPlugin.get_current().refresh_slide_cache(slide) as ret:
 		return ret
 
+# presenter state
+_state=_PresenterState()
